f7e1465703210af8baef96e972eae0153311aa74
Maciej Miś <macmisdev@gmail.com>
2015-03-02 20:38:46 +0100

    podstawowy konwerter obiekt-json dla publicznych typow prostych zrobiony

--- src/pl/edu/ug/inf/converter/JsonConverter.java
+++ src/pl/edu/ug/inf/converter/JsonConverter.java
@@ -1,7 +1,55 @@
 package pl.edu.ug.inf.converter;
 
-/**
- * Created by Zax on 2015-03-02.
- */
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class JsonConverter {
+
+    public JsonConverter() {
+    }
+
+    //Metoda konwertujaca obiekt na json
+    public String convertToJson(Object object){
+
+        Field[] fields = object.getClass().getDeclaredFields();
+        HashMap<String, String> map = getNamesAndValues(fields, object);
+
+        return generateJson(map);
+    }
+
+    //Metoda zwracajaca HashMapę, w której znajdują się nazwy pól wraz z ich wartościami
+    private HashMap getNamesAndValues(Field[] fields, Object object){
+
+        HashMap<String, String> values = new HashMap<String, String>();
+        for(Field f: fields){
+            //Jeżeli pole jest publiczne
+            if(f.getModifiers() == Modifier.PUBLIC){
+                try {
+                    //Dopisz do HashMapy nazwę zmiennej jako klucz oraz jej wartość
+                    values.put(f.getName(), f.get(object).toString());
+                } catch (IllegalAccessException e) {
+                    System.out.println("Pole prywatne, bądź chronione - brak dostępu!");
+                }
+            }
+        }
+        return values;
+    }
+    //Metoda generujaca jsona
+    private String generateJson(HashMap<String, String> map){
+        String json = "{ ";
+        Iterator iterator = map.entrySet().iterator();
+        //Przechodzimy iteratorem po HashMapie dopisując klucz oraz wartość do stringa
+        while(iterator.hasNext()){
+            Map.Entry entry = (Map.Entry) iterator.next();
+            json += '"'+ (String)entry.getKey() + '"' + " : " + '"' + entry.getValue() + '"';
+            if(iterator.hasNext()){
+               json += ", ";
+            }
+        }
+        json += "}";
+        return json;
+    }
 }
--- src/pl/edu/ug/inf/main/Main.java
+++ src/pl/edu/ug/inf/main/Main.java
@@ -1,8 +1,14 @@
 package pl.edu.ug.inf.main;
 
+import pl.edu.ug.inf.converter.JsonConverter;
+import pl.edu.ug.inf.pojo.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        Person Maciek = new Person("Maciej", "Miś", 24, 79.5);
+        JsonConverter converter = new JsonConverter();
+        System.out.println(converter.convertToJson(Maciek));
+
     }
 }
