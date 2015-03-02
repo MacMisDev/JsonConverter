45de8b943ed6694fdc9b116cb308c7aab9d1b639
Maciej Miś <macmisdev@gmail.com>
2015-03-02 18:46:37 +0100

    basic class, .gitignore

---
+++ src/pl/edu/ug/inf/converter/JsonConverter.java
@@ -0,0 +1,7 @@
+package pl.edu.ug.inf.converter;
+
+/**
+ * Created by Zax on 2015-03-02.
+ */
+public class JsonConverter {
+}
--- src/pl/edu/ug/inf/main/Main.java
+++ src/pl/edu/ug/inf/main/Main.java
@@ -1,4 +1,4 @@
-package pl.edu.ug.inf;
+package pl.edu.ug.inf.main;
 
 public class Main {
 
